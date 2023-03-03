ab8b96518884f6384b974a77250452c51b22bcd0
mamunkja <121738702+mamunkja@users.noreply.github.com>
2023-03-03 23:11:15 +0600

    app initialized

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Main from './Components/Main';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+import { BrowserRouter } from 'react-router-dom';
+
+function App() {
+  return (
+    <div>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Main />
+        </BrowserRouter>
+      </Provider>
+
+    </div>
+  );
+}
+
+export default App;
